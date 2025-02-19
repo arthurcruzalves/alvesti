ad9fdb25afc85cfbcb694eacd5d8f2a254b09450
ARTHUR CRUZ ALVES <arthur.calves2@senacsp.edu.br>
2025-02-19 17:19:59 -0300

    UC 12

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 console.log("Processo principal")
-const { app, BrowserWindow, nativeTheme} = require('electron')
+
+const { app, BrowserWindow, nativeTheme, Menu } = require('electron')
 
 // Janela principal
 let win
 const createWindow = () => {
     // a linha abaixo define o tema (claro ou escuro)
     nativeTheme.themeSource = 'light' // (dark ou light)
-  win = new BrowserWindow({
+    win = new BrowserWindow({
     width: 800,
     height: 600,
     //autoHideMenuBar: true,
@@ -14,7 +15,33 @@ const createWindow = () => {
     resizable: false
   })
 
+  // menu personalizado
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
   win.loadFile('./src/views/index.html')
+}
+
+//Janela sobre 
+function aboutWindow() {
+    nativeTheme.themeSource = 'light'  
+    // a linha abaixo obtém a janela principal
+    const main = BrowserWindow.getFocusedWindow()
+    let about
+    // Estabelecer uma relção hierárquica entre janelas
+    if (main) {
+      // Criar a janela sobre
+      about =  new BrowserWindow({
+        width: 360,
+        height: 200,
+        autoHideMenuBar: true,
+        resizable: false,
+        minimizable: false,
+        parent: main,
+        modal: true
+      })
+    }
+    //carregar o documento html na janela 
+    about.loadFile('./src/views/sobre.html')
 }
 
 // Iniciar a aplicação
@@ -33,3 +60,73 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+// reduzir logs não criticos
+app.commandLine.appendSwitch('log-level', '3')
+
+
+// templete do menu
+const template = [
+  {
+    label: 'Cadastro',
+    submenu: [
+      {
+        label: 'Cliente'
+      },
+      {
+        label: 'OS'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Sair',
+        click: () => app.quit(),
+        accelerator: 'Alt+F4'
+      }
+    ]
+  },
+  {
+    label: 'Relatórios'
+  },
+  {
+    label: 'Zoom'
+  },
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Aplicar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label: 'Reduzir',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Retaurar o zoom padrão',
+        role: 'resetZoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Reiniciar',
+        role: 'reload'
+      },
+      {
+        label: 'Ferramentas do desenvolvimento',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+    ]
+  }
+]
