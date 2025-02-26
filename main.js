dbf2c0a7d43af75345fd0d1c1fc5de6bbd661282
ARTHUR CRUZ ALVES <arthur.calves2@senacsp.edu.br>
2025-02-26 16:15:10 -0300

    janelas

--- main.js
+++ main.js
@@ -1,132 +1,193 @@
 console.log("Processo principal")
 
-const { app, BrowserWindow, nativeTheme, Menu } = require('electron')
+const { app, BrowserWindow, nativeTheme, Menu, ipcMain } = require('electron')
+
+const path = require('node:path')
 
 // Janela principal
 let win
 const createWindow = () => {
     // a linha abaixo define o tema (claro ou escuro)
-    nativeTheme.themeSource = 'light' // (dark ou light)
+    nativeTheme.themeSource = 'light' //(dark ou light)
     win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    //autoHideMenuBar: true,
-    //minimizable: false, 
-    resizable: false
-  })
+        width: 800,
+        height: 600,
+        //autoHideMenuBar: true,
+        //minimizable: false,
+        resizable: false,
+        //ativação do preload.js
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js')
+        }
+    })
 
-  // menu personalizado
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+    // menu personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-  win.loadFile('./src/views/index.html')
+    win.loadFile('./src/views/index.html')
+
+    // recebimento dos pedidos do renderizador para abertura de janelas (botões)
+    ipcMain.on('client-window', () => {
+      clientWindow()
+    })
+
+    ipcMain.on('os-window', () => {
+      osWindow()
+    })
 }
 
-//Janela sobre 
+// Janela sobre
 function aboutWindow() {
-    nativeTheme.themeSource = 'light'  
+    nativeTheme.themeSource = 'light'
     // a linha abaixo obtém a janela principal
     const main = BrowserWindow.getFocusedWindow()
     let about
-    // Estabelecer uma relção hierárquica entre janelas
+    // Estabelecer uma relação hierárquica entre janelas
     if (main) {
-      // Criar a janela sobre
-      about =  new BrowserWindow({
-        width: 360,
-        height: 200,
-        autoHideMenuBar: true,
-        resizable: false,
-        minimizable: false,
-        parent: main,
-        modal: true
-      })
+        // Criar a janela sobre
+        about = new BrowserWindow({
+            width: 360,
+            height: 220,
+            autoHideMenuBar: true,
+            resizable: false,
+            minimizable: false,
+            parent: main,
+            modal: true
+        })
     }
-    //carregar o documento html na janela 
+    //carregar o documento html na janela
     about.loadFile('./src/views/sobre.html')
+}
+
+// Janela cliente
+let client
+function clientWindow() {
+    nativeTheme.themeSource ='light'
+    const main = BrowserWindow.getFocusedWindow()
+    if(main) {
+        client = new BrowserWindow({
+            width: 1010,
+            height: 720,
+            //
+            autoHideMenuBar: true,
+            resizable: false,
+            parent: main,
+            modal: true
+        })
+    }
+  client.loadFile('./src/views/cliente.html')
+}
+
+// Janela OS
+let OS
+function osWindow() {
+    nativeTheme.themeSource ='light'
+    const main = BrowserWindow.getFocusedWindow()
+    if(main) {
+        OS = new BrowserWindow({
+            width: 1010,
+            height: 720,
+            autoHideMenuBar: true,
+            resizable: false,
+            parent: main,
+            modal: true
+        })
+    }
+  OS.loadFile('./src/views/os.html')
 }
 
 // Iniciar a aplicação
 app.whenReady().then(() => {
-  createWindow()
-})
+    createWindow()
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-// reduzir logs não criticos
+//reduzir logs não críticos
 app.commandLine.appendSwitch('log-level', '3')
 
-
-// templete do menu
+// template do menu
 const template = [
-  {
-    label: 'Cadastro',
-    submenu: [
-      {
-        label: 'Cliente'
-      },
-      {
-        label: 'OS'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Sair',
-        click: () => app.quit(),
-        accelerator: 'Alt+F4'
-      }
-    ]
-  },
-  {
-    label: 'Relatórios'
-  },
-  {
-    label: 'Zoom'
-  },
-  {
-    label: 'Ferramentas',
-    submenu: [
-      {
-        label: 'Aplicar zoom',
-        role: 'zoomIn'
-      },
-      {
-        label: 'Reduzir',
-        role: 'zoomOut'
-      },
-      {
-        label: 'Retaurar o zoom padrão',
-        role: 'resetZoom'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Reiniciar',
-        role: 'reload'
-      },
-      {
-        label: 'Ferramentas do desenvolvimento',
-        role: 'toggleDevTools'
-      }
-    ]
-  },
-  {
-    label: 'Ajuda',
-    submenu: [
-      {
-        label: 'Sobre',
-        click: () => aboutWindow()
-      }
-    ]
-  }
+    {
+        label: 'Cadastro',
+        submenu: [
+            {
+                label: 'Clientes',
+                click: () => clientWindow()
+            },
+            {
+                label: 'OS',
+                click: () => osWindow()
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label: 'Relatórios',
+        submenu: [
+            {
+                label: 'Clientes'
+            },
+            {
+                label: 'OS abertas'
+            },
+            {
+                label: 'OS concluídas'
+            }
+        ]
+    },
+    {
+        label: 'Ferramentas',
+        submenu: [
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrão',
+                role: 'resetZoom'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Recarregar',
+                role: 'reload'
+            },
+            {
+                label: 'Ferramentas do desenvolvedor',
+                role: 'toggleDevTools'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Sobre',
+                click: () => aboutWindow()
+            }
+        ]
+    }
 ]
